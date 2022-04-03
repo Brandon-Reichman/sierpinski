0e5d1744e6cff4463fbab000fb1083393c5fd637
Brandon-Reichman <reichman.brandon@gmail.com>
2022-04-03 15:51:02 -0500

    move sierpinski triangle methods to service file

--- src/components/SierpinskiTriangle.vue
+++ src/components/SierpinskiTriangle.vue
@@ -1,111 +1,47 @@
 <template>
     <div class="wrapper">
-        <canvas id="triangle" width="1200" height="1200"></canvas>
+        <h2 v-if="!insideTriangle" style="color:red;">Please chose a point inside of the triangle</h2>
+        <canvas style="border:solid 1px blue;" @click="clickTriangle" id="triangle" width="1200" height="1200"></canvas>
     </div>
 </template>
 
 <script>
+import sierpinski from "@/service/sierpinski";
+
 export default {
     name: "SierpinskiTriangle",
-    computed: {
-        coordinates() {
-            let side = 1000;
-
-            let h = side * (Math.sqrt(3)/2);
-
-            return {
-                a: { x: 0, y: - h / 2 },
-                b: { x: -side / 2, y: h / 2 },
-                c: { x: side / 2, y: h / 2 },
-            }
-        },
-    },
+    mixins: [sierpinski],
     methods: {
-        drawTriangle(ctx, cx, cy) {
-
-            ctx.strokeStyle = "#ff0000";
-
-            ctx.save();
-            ctx.translate(cx, cy);
-
-            ctx.moveTo(this.coordinates.a.x, this.coordinates.a.y);
-            ctx.lineTo( this.coordinates.b.x, this.coordinates.b.y);
-            ctx.lineTo(this.coordinates.c.x, this.coordinates.c.y);
-            ctx.lineTo(this.coordinates.a.x, this.coordinates.a.y);
-
-            ctx.stroke();
-
-            ctx.closePath();
-            ctx.save();
-        },
-        getNewPoint() {
-            let r1 = Math.random();
-            let r2 = Math.random();
-            let point = {
-                x: (1 - Math.sqrt(r1)) * this.coordinates.a.x + (Math.sqrt(r1) * (1 - r2)) * this.coordinates.b.x + (Math.sqrt(r1) * r2) * this.coordinates.c.x,
-                y: (1 - Math.sqrt(r1)) * this.coordinates.a.y + (Math.sqrt(r1) * (1 - r2)) * this.coordinates.b.y + (Math.sqrt(r1) * r2) * this.coordinates.c.y,
-            }
-            if(this.checkPoint(point)){
-                return point;
-            } else {
-                return this.getNewPoint();
+        async clickTriangle(e) {
+            if(this.insideTriangle === false ){
+                this.clicked = true;
+                let canvas = document.querySelector("#triangle");
+                let ctx = canvas.getContext("2d");
+                let rect = canvas.getBoundingClientRect();
+                let position = {
+                    x: e.clientX - rect.left - (canvas.width / 2),
+                    y: e.clientY - rect.bottom + (canvas.height / 2),
+                }
+                if(sierpinski.checkPoint(position)){
+                    this.insideTriangle = true;
+                    ctx.fillRect(position.x, position.y,3,3);
+                    await sierpinski.drawMidPoint(ctx, position);
+                } else {
+                    this.insideTriangle = false;
+                }
             }
         },
-        checkPoint(checkPoint) {
-            let A = this.triangleArea(this.coordinates.a, this.coordinates.b, this.coordinates.c);
-            let A1 = this.triangleArea(checkPoint, this.coordinates.b, this.coordinates.c);
-            let A2 = this.triangleArea(this.coordinates.a, checkPoint, this.coordinates.c);
-            let A3 = this.triangleArea(this.coordinates.a, this.coordinates.b, checkPoint);
-
-            return (A === A1 + A2 + A3);
-        },
-        triangleArea(a, b, c) {
-            return Math.abs(
-                (a.x * (b.y - c.y) +
-                    b.x * (c.y - a.y) +
-                    c.x * (a.y - b.y)) / 2
-            );
-        },
-        drawPoint (ctx) {
-            let newPoint = this.getNewPoint();
-            ctx.fillRect(newPoint.x, newPoint.y,3,3);
-            return newPoint;
-
-        },
-        async drawMidPoint(ctx, point) {
-            let randC = this.getRandomCoordinate();
-            let newPoint = {
-                x: (randC.x + point.x) / 2,
-                y: (randC.y + point.y) / 2,
-            }
-            ctx.fillRect(newPoint.x, newPoint.y, 3, 3);
-            await this.sleep(25);
-            await this.drawMidPoint(ctx, newPoint);
-        },
-        getRandomCoordinate() {
-            let rand = Math.floor(Math.random() * 3);
-            switch(rand)
-            {
-                case 0:
-                    return this.coordinates.a;
-                case 1:
-                    return this.coordinates.b;
-                case 2:
-                    return this.coordinates.c;
-            }
-        },
-        sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
     },
     async mounted() {
         let canvas = document.querySelector("#triangle");
         let ctx = canvas.getContext("2d");
-        this.drawTriangle(ctx, canvas.width/2, canvas.height/2);
-        let firstPoint = this.drawPoint(ctx);
-        await this.sleep(25);
-        await this.drawMidPoint(ctx, firstPoint);
-
+        sierpinski.drawTriangle(ctx, canvas.width/2, canvas.height/2);
+    },
+    data() {
+        return {
+            delay: sierpinski.delay,
+            insideTriangle: false,
+        }
     }
 }
 </script>
---
+++ src/service/sierpinski.js
@@ -0,0 +1,90 @@
+let side = 1000;
+let h = side * (Math.sqrt(3)/2);
+
+let delay = 25;
+const coordinates = {
+	a: { x: 0, y: - h / 2 },
+	b: { x: -side / 2, y: h / 2 },
+	c: { x: side / 2, y: h / 2 },
+};
+
+const sierpinski = {
+	drawTriangle(ctx, cx, cy) {
+		ctx.strokeStyle = "#ff0000";
+
+		ctx.save();
+		ctx.translate(cx, cy);
+
+		ctx.moveTo(coordinates.a.x, coordinates.a.y);
+		ctx.lineTo(coordinates.b.x, coordinates.b.y);
+		ctx.lineTo(coordinates.c.x, coordinates.c.y);
+		ctx.lineTo(coordinates.a.x, coordinates.a.y);
+
+		ctx.stroke();
+
+		ctx.closePath();
+		ctx.save();
+	},
+	drawPoint (ctx) {
+		let newPoint = this.getNewPoint();
+		ctx.fillRect(newPoint.x, newPoint.y,3,3);
+		return newPoint;
+	},
+	async drawMidPoint(ctx, point) {
+		let randC = this.getRandomCoordinate();
+		let newPoint = {
+			x: (randC.x + point.x) / 2,
+			y: (randC.y + point.y) / 2,
+		}
+		ctx.fillRect(newPoint.x, newPoint.y, 3, 3);
+		await this.sleep();
+		await this.drawMidPoint(ctx, newPoint);
+	},
+	getNewPoint() {
+		let r1 = Math.random();
+		let r2 = Math.random();
+		let point = {
+			x: (1 - Math.sqrt(r1)) * this.coordinates.a.x + (Math.sqrt(r1) * (1 - r2)) * this.coordinates.b.x + (Math.sqrt(r1) * r2) * this.coordinates.c.x,
+			y: (1 - Math.sqrt(r1)) * this.coordinates.a.y + (Math.sqrt(r1) * (1 - r2)) * this.coordinates.b.y + (Math.sqrt(r1) * r2) * this.coordinates.c.y,
+		}
+		if(this.checkPoint(point)){
+			return point;
+		} else {
+			return this.getNewPoint();
+		}
+	},
+	getRandomCoordinate() {
+		let rand = Math.floor(Math.random() * 3);
+		switch(rand)
+		{
+			case 0:
+				return this.coordinates.a;
+			case 1:
+				return this.coordinates.b;
+			case 2:
+				return this.coordinates.c;
+		}
+	},
+	sleep(ms = this.delay) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	},
+	checkPoint(checkPoint) {
+		let A = this.triangleArea(this.coordinates.a, this.coordinates.b, this.coordinates.c);
+		let A1 = this.triangleArea(checkPoint, this.coordinates.b, this.coordinates.c);
+		let A2 = this.triangleArea(this.coordinates.a, checkPoint, this.coordinates.c);
+		let A3 = this.triangleArea(this.coordinates.a, this.coordinates.b, checkPoint);
+
+		return (A === A1 + A2 + A3);
+	},
+	triangleArea(a, b, c) {
+		return Math.abs(
+			(a.x * (b.y - c.y) +
+				b.x * (c.y - a.y) +
+				c.x * (a.y - b.y)) / 2
+		);
+	},
+	coordinates,
+	delay,
+}
+
+export default sierpinski;
