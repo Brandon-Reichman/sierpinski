b9a8edb92a717a27ed84cd957c6adf8764644b7d
Brandon-Reichman <reichman.brandon@gmail.com>
2022-04-03 19:33:55 -0500

    added clear button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-    <SierpinskiTriangle />
+    <div id="app">
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <SierpinskiTriangle />
+    </div>
 </template>
 
 <script>
--- src/components/SierpinskiTriangle.vue
+++ src/components/SierpinskiTriangle.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="wrapper">
         <form class="param-form" @submit.prevent="" @change="handleChange">
+            <input type="range" min="0" max="5000" v-model="delay" />
             <input type="number" v-model="delay" />
             <label> Speed (ms / dot)</label>
             <span class="form-space"></span>
+            <input type="range" min="0" max="10" v-model="size" />
             <input type="number" v-model="size" />
-            <label> Dot Size (px)</label>
+            <label> {{ size }} Dot Size (px)</label>
         </form>
         <canvas style="border:solid 1px blue;" id="triangle" width="1200" height="1200"></canvas>
+    </div>
+    <div>
+        <button @click="clearCanvas" class="button">Clear</button>
     </div>
     <div>
         <button @click="buttonClick('stop')" class="button stop-button">Stop</button>
@@ -21,14 +26,20 @@ import sierpinski from "@/service/sierpinski";
 export default {
     name: "SierpinskiTriangle",
     mixins: [sierpinski],
+    computed: {
+        canvas() {
+            return document.querySelector("#triangle");
+        },
+        ctx() {
+            return this.canvas.getContext("2d");
+        }
+    },
     methods: {
         handleChange() {
             sierpinski.delay = this.delay;
             sierpinski.size = this.size;
         },
         async buttonClick(type) {
-            let canvas = document.querySelector("#triangle");
-            let ctx = canvas.getContext("2d");
             switch(type)
             {
                 case 'stop':
@@ -36,17 +47,18 @@ export default {
                     break;
                 case 'start':
                     if(sierpinski.lastPoint === null) {
-                        sierpinski.drawPoint(ctx);
+                        sierpinski.drawPoint(this.ctx);
                     }
                     sierpinski.stop = false;
-                    await sierpinski.drawMidPoint(ctx, sierpinski.lastPoint);
+                    await sierpinski.drawMidPoint(this.ctx, sierpinski.lastPoint);
             }
+        },
+        clearCanvas(){
+            window.location.href = '/';
         },
     },
     async mounted() {
-        let canvas = document.querySelector("#triangle");
-        let ctx = canvas.getContext("2d");
-        sierpinski.drawTriangle(ctx, canvas.width/2, canvas.height/2);
+        sierpinski.drawTriangle(this.ctx, this.canvas.width/2, this.canvas.height/2);
     },
     data() {
         return {
